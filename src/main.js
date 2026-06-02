0603b4e786b276f241b519e3d9840eccc4788839
reham <rehamm6601@gmail.com>
2026-06-02 09:46:55 +0300

    fix router

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
-
 import { createApp } from 'vue'
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 import './style.css'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: [
-    { path: '/', component: () => import('./components/HeroSection.vue') },
-    { path: '/menu', component: () => import('./components/MenuSection.vue') },
+    { path: '/',        component: () => import('./components/HeroSection.vue') },
+    { path: '/menu',    component: () => import('./components/MenuSection.vue') },
+    { path: '/about',   component: () => import('./components/AboutPage.vue') },
+    { path: '/contact', component: () => import('./components/ContactPage.vue') },
   ],
-  scrollBehavior() {
-    return { top: 0 }
-  }
+  scrollBehavior() { return { top: 0 } }
 })
 
 createApp(App).use(router).mount('#app')
